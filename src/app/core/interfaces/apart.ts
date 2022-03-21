06b61d909fd07ec3396fdc76083b6aca58454c31
Florian Cadoz <fcadoz@waycom.net>
2022-03-21 23:03:10 +0100

    Form Template Started

--- src/app/core/interfaces/apart.ts
+++ src/app/core/interfaces/apart.ts
@@ -1,8 +1,11 @@
 export interface IApart {
+  _id: string;
   name: string;
   description: string;
   image: string;
   comments: IComment[];
+  lat: number;
+  lon: number;
 }
 
 interface IComment {
--- src/app/views/form/form.component.ts
+++ src/app/views/form/form.component.ts
@@ -11,21 +11,35 @@ import { IApart } from '@core/interfaces';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  public loading: boolean;
+  public apart!: IApart;
 
   constructor(
     private _router: Router,
     private _apartService: ApartService
-  ) { }
+  ) {
+    this.loading = false;
+  }
 
   ngOnInit(): void {
     this._getApart();
   }
 
+  public save() {
+    console.log('appart: ', this.apart);
+  }
+
   private _getApart(): void {
+    this.loading = true;
     const name = this._router.url.replace('/apartments/','');
+
     this._apartService.getApart(name)
       .subscribe(
-        (res: IApart) => console.log(res),
+        (res: IApart) => {
+          console.log(res);
+          this.apart = res;
+          this.loading = false;
+        },
         err => {
           if (err instanceof HttpErrorResponse) {
             if (err.status === 401) {
