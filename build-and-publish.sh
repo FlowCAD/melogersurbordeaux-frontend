6046faface31217d1bbf530bd0b8ec47d8372c64
Florian Cadoz <fcadoz@waycom.net>
2022-05-02 11:11:54 +0200

    Script for building and publishing added

---
+++ build-and-publish.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "---------------------------------------------------------\n"
+echo "------------RUNNING BUILD AND PUBLISH SCRIPT!------------\n"
+echo "---------------------------------------------------------\n"
+
+echo "\n---> Cleaning output folder\n"
+cd ../backend/public/
+rm -rf *
+cd -
+
+echo "\n---> Building Angular Package\n"
+ng build --c=production --delete-output-path
+
+echo "\n---> Moving Package's Files\n"
+cp -R ./dist/melogersurbordeaux/. ../backend/public/
+
+echo "\n---> Publishing Package\n"
+cd ../backend
+git add .
+git commit -am "Updated public files"
+git push
+git push heroku master
+
+echo "\n----Package successfully built and published ! \o/ ----\n"
